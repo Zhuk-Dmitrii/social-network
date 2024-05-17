38abf22aa98bf26fa695f0e6df4ff8c0f5a642be
Zhuk-Dmitrii <fgh298@rambler.ru>
2024-05-17 19:22:01 +0300

    refact vite.config.ts css modules

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['**/*.scss'],
+  },
   css: {
     modules: {
-      localsConvention: 'camelCase',
       generateScopedName: '[folder]__[local]__[hash:base64:2]',
     },
   },
