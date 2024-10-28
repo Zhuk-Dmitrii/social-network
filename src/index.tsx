2beb84707fdad7fe8f9b6e4d9088eefb3f4353ae
Zhuk-Dmitrii <fgh298@rambler.ru>
2024-10-28 13:19:09 +0300

    refactor: changed file name "state" to "stor"

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 
-import { store } from './redux/state.ts'
+import { store } from './redux/store.ts'
 import { App } from './App.tsx'
 import { TState } from './types/type.ts'
 
